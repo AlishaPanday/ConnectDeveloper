388c10201c795f15244066d3408467f96fa131b6
Alisha Panday <panday.alisha90@gmail.com>
2021-05-03 12:16:55 +0800

    connecting database

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+//import express
 const express = require('express');
+const dbConnection = require('./config/db');
+
 //initialise app variable with express
 const app = express();
+
+//connecting  database
+dbConnection();
 
 app.get('/',(req,res) => res.send('API Server Running'));
 //process.env will look for an evironment variable called PORT when deployed in HEROKU
