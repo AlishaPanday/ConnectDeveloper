54d023632cca502ff29ff2064edaa4fa9c26d673
Alisha Panday <panday.alisha90@gmail.com>
2021-05-03 17:37:03 +0800

    useNewUrlParser, useUnifiedTopology, useCreateIndex set to true

--- config/db.js
+++ config/db.js
@@ -2,18 +2,23 @@
 const mongoose = require('mongoose');
 //require config
 const config = require('config');
-//import string which is inside db.json 
+//import string which is inside db.json
 const db = config.get('mongoURI');
 
 //connecting to mongodb using mongoose (async/await)
 const dbConnection = async () => {
-    try{
-        await mongoose.connect(db);
-        console.log('Connected to MongoDB');
-    } catch(err) {
-        console.error(err.message);
-        // exit the process if something goes wrong with failure
-        process.exit(1);
-    }
-}
+  try {
+    await mongoose.connect(db, {
+           useNewUrlParser:true,
+           useCreateIndex:true,
+           useUnifiedTopology: true,
+           useFindAndModify: false
+    });
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err.message);
+    // exit the process if something goes wrong with failure
+    process.exit(1);
+  }
+};
 module.exports = dbConnection;
