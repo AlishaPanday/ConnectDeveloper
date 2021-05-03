557e9bd2a125cc8465286afc28ff26dac7771016
Alisha Panday <panday.alisha90@gmail.com>
2021-05-03 19:33:51 +0800

    imported auth middleware to the auth routes and access verification finding userId

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,4 +1,4 @@
-//handle json webtoken for authentication 
+//handle json webtoken for authentication
 //handles user registration
 //import express
 const express = require('express');
@@ -6,7 +6,21 @@ const express = require('express');
 //use express router
 const router = express.Router();
 
-//GET api/auth 
-router.get('/', (req,res) => res.send('Autho route'));
+//import middleware
+const auth = require('../../middleware/auth');
+
+//import model
+const User = require('../../models/User');
+
+//GET api/auth
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
