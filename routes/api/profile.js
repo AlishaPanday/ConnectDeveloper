6499ea17484261ceeb073b6031a84c4036944357
Alisha Panday <panday.alisha90@gmail.com>
2021-05-04 11:06:46 +0800

    Fixed path error when importing model

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,10 +3,32 @@
 //import express
 const express = require('express');
 
+const auth = require('../../middleware/auth');
+
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
 //use express router
 const router = express.Router();
 
-//GET api/Profile
-router.get('/', (req,res) => res.send('Profile route'));
+//GET api/Profile/personal
+//Get current user profile with private access via token
+router.get('/personal', auth, async (req,res) => {
+   try {
+    const userProfile = await Profile.findOne({user:req.user.id}).populate('user',
+    ['name','avatar']);
+
+    //check if there is no user profile
+    if(!userProfile) {
+        res.status(400).json({msg:'Profile does not exist for this user'})
+    }
+
+    res.json(userProfile);
+   }catch(err){
+    console.error(err.message);
+    res.status(500).send('Server Error 500');
+   }
+    
+});
 
 module.exports = router;
