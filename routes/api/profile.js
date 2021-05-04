10c3b5e19df449dc6be857efae5304be7956f68c
Alisha Panday <panday.alisha90@gmail.com>
2021-05-04 18:39:51 +0800

    routes to get user github repos

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,11 +1,12 @@
 //profile create and update operations
-//import express,auth middleware, models
+//import express,auth middleware, models, request, config
 const express = require('express');
 const auth = require('../../middleware/auth');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 const { check, validationResult } = require('express-validator');
-
+const request = require('request');
+const config = require('config');
 //use express router
 const router = express.Router();
 
@@ -311,4 +312,32 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+//get api/profile/github/:username to get user repos from github
+
+router.get('/github/:username',(req,res)=>{
+  try{
+    const options = {
+      uri:`https://api.github.com/users/${req.params.username}/repos?per_page=5&
+      sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=
+      ${config.get('githubSecret')}`,
+      method:'GET',
+      headers:{'user-agent':'node.js'}
+
+    };
+
+    request(options,(error,response,body) => {
+      if(error) console.error(error);
+      if(response.statusCode !== 200){
+        return res.status(400).json({msg:'No Github profile found'});
+      }
+
+      res.json(JSON.parse(body));
+
+    });
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send('Internal Server Error');
+
+  }
+})
 module.exports = router;
