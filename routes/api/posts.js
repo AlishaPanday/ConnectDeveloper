8dc34f6bcaa1d8de7d1cd7272e1604b636fdf1f0
Alisha Panday <panday.alisha90@gmail.com>
2021-05-05 12:27:44 +0800

    Post request to add new post by user added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,49 @@
-// Post/like/comments
-//handles user registration
-//import express
+//handles user's Post/like & Comments
+
+//import express,auth middleware, express-validator
 const express = require('express');
-
-//use express router
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
+// const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const Post = require('../../models/Post');
 
-//GET api/posts
-router.get('/', (req,res) => res.send('Posts route'));
+//Post request to private api/posts
+router.post(
+  '/',
+  [
+    auth,
+    [
+      //check for text field
+      check('text', 'Text is required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    //error checking
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const addPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const newPost = await addPost.save();
+      //send Json response
+      res.json(newPost);
+
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Internal Server Error');
+    }
+  }
+);
 
 module.exports = router;
