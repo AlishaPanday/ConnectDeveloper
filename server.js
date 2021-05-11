80bc0b384ddd8458a1c9b611a1786c393cf759f4
Alisha <panday.alisha90@gmail.com>
2021-05-11 20:18:17 +0800

    Heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //import express
 const express = require('express');
 const dbConnection = require('./config/db');
-
+const path = require('path');
 
 //initialise app variable with express
 const app = express();
@@ -13,14 +13,22 @@ dbConnection();
 app.use(express.json({extended:true}));
 
 
-
-app.get('/',(req,res) => res.send('API Server Running'));
-
 //routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 //process.env will look for an evironment variable called PORT when deployed in HEROKU
 const PORT = process.env.PORT || 5000;
 
